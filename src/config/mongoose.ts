1d353cbe0b7db2c0bd7f05e1dcd33b7f7d1f9f67
Rodolfo Silva <contato@rodolfosilva.com>
2018-10-21 14:07:41 -0300

    Remove unnecessary check

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -6,31 +6,27 @@ const debug: IDebugger = createDebug('server');
 
 mongoose.Promise = global.Promise;
 
-if (!vars.isTest) {
-  mongoose.connection.on('connected', () =>
-    debug(`Mongoose default connection open to ${vars.mongo}`),
-  );
+mongoose.connection.on('connected', () =>
+  debug(`Mongoose default connection open to ${vars.mongo}`),
+);
 
-  mongoose.connection.on('disconnected', () =>
-    debug('Mongoose default connection disconnected'),
-  );
+mongoose.connection.on('disconnected', () =>
+  debug('Mongoose default connection disconnected'),
+);
 
-  mongoose.connection.on('error', error => {
-    debug(`Mongoose default connection error: ${error}`);
-    process.exit(-1);
+mongoose.connection.on('error', error => {
+  debug(`Mongoose default connection error: ${error}`);
+  process.exit(-1);
+});
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    debug('Mongoose default connection disconnected through app termination');
+    process.exit(0);
   });
+});
 
-  process.on('SIGINT', () => {
-    mongoose.connection.close(() => {
-      debug('Mongoose default connection disconnected through app termination');
-      process.exit(0);
-    });
-  });
-}
-
-if (vars.isDevelopment) {
-  mongoose.set('debug', true);
-}
+mongoose.set('debug', vars.isDevelopment);
 
 mongoose.set('useCreateIndex', true);
 
